9c83272d9c79346303af8086ace49a07c1f52253
fj0lverkr <nils.nahooy@live.com>
2021-02-07 16:56:58 +0100

    colors and layout done, boring and veryboring options done

--- data/WeatherData.py
+++ data/WeatherData.py
@@ -1,7 +1,7 @@
 from requests import get
 from datetime import datetime
 
-from data.constants import WEATHER_ICONS as IC
+from data.constants import WEATHER_ICONS as IC, COLORS
 
 
 class WeatherDataProvider:
@@ -17,10 +17,15 @@ class WeatherDataProvider:
 
 
 class WeatherDataParser:
-    def __init__(self, json_data, u, tf):
+    def __init__(self, json_data, u, tf, color_off, color_icons_off):
         self.data = json_data
         self.units = u
-        self.timeformat = '%H:%M' if tf == "24" else '%I:%M %p'
+        self.color_off = color_off
+        self.color_icons_off = color_icons_off
+        if self.color_icons_off:
+            self.timeformat = '%H:%M' if tf == "24" else '%I:%M %p'
+        else:
+            self.timeformat = '%H[blink]:[/blink]%M' if tf == "24" else '%I[blink]:[/blink]%M %p'
 
     def parse_data(self):
         location = self.data['name']
@@ -39,7 +44,8 @@ class WeatherDataParser:
         temp, feels_like, min, max, pressure, humidity = self.data['main'].values(
         )
         visibility = self.data['visibility']
-        beaufort = self.convert_to_beaufort(float(self.data['wind']['speed']))
+        self.beaufort = self.convert_to_beaufort(
+            float(self.data['wind']['speed']))
         wind_direction = self.convert_to_cardinal_direction(
             float(self.data['wind']['deg']))
 
@@ -84,52 +90,91 @@ class WeatherDataParser:
             snow_1h = -1
             snow_3h = -1
 
-        # Icons
+        # Icons and colors
         temp_icon = IC['units'][temp_unit]
         sunrise_icon, sunset_icon = IC['time'].values()
         feels_icon, max_icon, min_icon, press_icon, humid_icon, visibility_icon = IC['misc'].values(
         )
-        beaufort_icon = IC['wind']['speed'][beaufort]
+        beaufort_icon = IC['wind']['speed'][self.beaufort]
         wind_direction_icon = IC['wind']['direction'][wind_direction]
 
         if 199 < weather_icon_id < 233:
             weather_icon = IC[day_night]['thunderstorm']
+            descr_color = COLORS['stormy']
         elif (299 < weather_icon_id < 322) or 499 < weather_icon_id < 532:
             weather_icon = IC[day_night]['rain']
+            descr_color = COLORS['rainy']
             if rain_1h > 0 and rain_3h > 0:
                 weather_description += f" ({rain_1h} mm/1h, {rain_3h} mm/3h)"
             elif rain_1h > 0:
                 weather_description += f" ({rain_1h} mm/1h)"
         elif 599 < weather_icon_id < 623:
             weather_icon = IC[day_night]['snow']
+            descr_color = COLORS['snowy']
             if snow_1h > 0 and snow_3h > 0:
                 weather_description += f" ({snow_1h} mm/1h, {snow_3h} mm/3h)"
             elif snow_1h > 0:
                 weather_description += f" ({snow_1h} mm/1h)"
         elif 700 < weather_icon_id < 782:
             weather_icon = IC[day_night]['mist']
+            descr_color = COLORS['misty']
         elif weather_icon_id == 800:
             weather_icon = IC[day_night]['clear_sky']
+            descr_color = COLORS['clear'][day_night]
         elif weather_icon_id == 801:
             weather_icon = IC[day_night]['few_clouds']
+            descr_color = COLORS['cloudy']
             if clouds_pct > 0:
                 weather_description += f" ({clouds_pct}%)"
         elif weather_icon_id == 802:
             weather_icon = IC[day_night]['scattered_clouds']
+            descr_color = COLORS['cloudy']
             if clouds_pct > 0:
                 weather_description += f" ({clouds_pct}%)"
         elif weather_icon_id in [803, 804]:
             weather_icon = IC[day_night]['broken_clouds']
+            descr_color = COLORS['cloudy']
             if clouds_pct > 0:
                 weather_description += f" ({clouds_pct}%)"
         else:
             weather_icon = IC[day_night]['clear_sky']
+            descr_color = COLORS['clear'][day_night]
+
+        base_color = COLORS['main'][day_night]
+        temp_color = self.color_code_temp(temp)
+        feels_color = self.color_code_temp(feels_like)
+        min_color = self.color_code_temp(min)
+        max_color = self.color_code_temp(max)
+        bft_color = self.color_code_bf()
+
+        # Parse substrings
+        if self.color_off:
+            location = f" {location}"
+            time = f"{current_time_pretty} ({sunrise_icon}{sunrise_pretty} {sunset_icon}{sunset_pretty}):"
+            descr = f"\n {weather_icon}{weather_description},"
+            temp = f"{temp}{temp_icon}({feels_icon} {feels_like}{temp_icon} "
+            temp += f"{min_icon} {min}{temp_icon} "
+            temp += f"{max_icon} {max}{temp_icon})"
+            extras = f"\n {press_icon}{pressure}hPa, {humid_icon} {humidity}%, {visibility_icon} {visibility}m, {beaufort_icon} {self.beaufort} BFT {wind_direction_icon}"
+        elif self.color_icons_off:
+            location = f" {location}"
+            time = f"{current_time_pretty} ({sunrise_pretty} - {sunset_pretty}):"
+            descr = f"\n {weather_description},"
+            temp = f"{temp}{temp_icon}(feels like {feels_like}{temp_icon}, "
+            temp += f"min. {min}{temp_icon}, "
+            temp += f"max. {max}{temp_icon})"
+            extras = f"\n {pressure}hPa, {humidity}%, {visibility}m, {self.beaufort} BFT {wind_direction}"
+        else:
+            location = f" [{base_color}]{location}"
+            time = f"{current_time_pretty} ([{COLORS['clear']['day']}]{sunrise_icon}{sunrise_pretty}[/{COLORS['clear']['day']}] [{COLORS['sunset']}]{sunset_icon}{sunset_pretty}[/{COLORS['sunset']}]):[/{base_color}]"
+            descr = f"\n [{descr_color}]{weather_icon}{weather_description},[/{descr_color}]"
+            temp = f"[{temp_color}]{temp}{temp_icon}([{feels_color}]{feels_icon} {feels_like}{temp_icon}[/{feels_color}] "
+            temp += f"[{min_color}]{min_icon} {min}{temp_icon}[/{min_color}] "
+            temp += f"[{max_color}]{max_icon} {max}{temp_icon}[/{max_color}])[/{temp_color}]"
+            extras = f"\n [{base_color}]{press_icon}{pressure}hPa, {humid_icon} {humidity}%, {visibility_icon} {visibility}m, [{bft_color}]{beaufort_icon} {self.beaufort} BFT {wind_direction_icon}[/{bft_color}][/{base_color}]"
 
         # Parse String
-        weather = f" {location} {current_time_pretty} ({sunrise_icon}{sunrise_pretty} - {sunset_icon}{sunset_pretty}):"
-        weather += f"\n {weather_icon}{weather_description}, {temp}{temp_icon} ({feels_icon} {feels_like}{temp_icon}, {min_icon} {min}{temp_icon} - {max_icon} {max}{temp_icon})"
-        weather += f"\n {press_icon}{pressure}hPa, {humid_icon} {humidity}%, {visibility_icon} {visibility}m, {beaufort_icon} {beaufort} BFT {wind_direction_icon}"
-        weather += "\n"
+        weather = f"[bold]{location} {time}{descr} {temp}{extras}[/bold]\n"
 
         return weather
 
@@ -222,3 +267,51 @@ class WeatherDataParser:
             return "NW"
         else:
             return "NNW"
+
+    def color_code_temp(self, temp):
+        temp = float(temp)
+        if self.units == "imperial":
+            if 32.0 < temp < 60.8:
+                color = 'cold'
+            elif 60.8 <= temp <= 77.0:
+                color = 'nice'
+            elif 77.0 < temp <= 95.0:
+                color = 'warm'
+            elif 95.0 < temp:
+                color = 'hot'
+            elif temp <= 32.0:
+                color = 'freezing'
+        elif self.units == "metric":
+            if 0.0 < temp < 16.0:
+                color = 'cold'
+            elif 16.0 <= temp <= 25.0:
+                color = 'nice'
+            elif 25.0 < temp <= 35.0:
+                color = 'warm'
+            elif 35.0 < temp:
+                color = 'hot'
+            elif temp <= 0:
+                color = 'freezing'
+        else:
+            if 273.0 < temp < 289.15:
+                color = 'cold'
+            elif 289.15 <= temp <= 298.15:
+                color = 'nice'
+            elif 298.15 < temp <= 308.15:
+                color = 'warm'
+            elif 308.0 < temp:
+                color = 'hot'
+            elif temp <= 273.0:
+                color = 'freezing'
+        return COLORS['temp'][color]
+
+    def color_code_bf(self):
+        if 0 <= self.beaufort < 6:
+            color = 'calm'
+        elif 6 <= self.beaufort < 8:
+            color = 'mild'
+        elif 8 <= self.beaufort < 10:
+            color = 'rough'
+        else:
+            color = 'danger'
+        return COLORS['wind'][color]
